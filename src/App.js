1a2919cd042a78e97956a23a23dd8bac5e8dc8f5
Tobias Graf <tobias.graf@4com.de>
2020-10-12 14:50:16 +0200

    lol heroe list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
+function SearchField({ onSubmit }) {
+  const [value, setValue] = React.useState("");
+
+  return (
+    <form
+      className="search-field"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(value);
+      }}
+    >
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      <button type="submit">Filter</button>
+    </form>
+  );
+}
+
+const Heroe = ({ name, tags }) => {
+  const [owned, setOwned] = React.useState(false);
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{tags.join(", ")}</td>
+      <td>
+        <input
+          type="checkbox"
+          checked={owned}
+          onChange={() => setOwned(!owned)}
+        ></input>
+      </td>
+    </tr>
+  );
+};
+
+function HeroeList({ heroes }) {
+  const [value, setValue] = React.useState("");
+
+  return (
+    <div className="heroe-list">
+      <SearchField onSubmit={(value) => setValue(value)} />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Tags</th>
+            <th>Owned</th>
+          </tr>
+        </thead>
+        <tbody>
+          {heroes
+            .filter((heroe) =>
+              heroe.name.toLowerCase().includes(value.toLowerCase())
+            )
+            .map((heroe) => (
+              <Heroe name={heroe.name} tags={heroe.tags}></Heroe>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
 function App() {
+  const [heroes, setHeroes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ngryman/lol-champions/master/champions.json"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setHeroes(json);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
-
-        <div></div>
-      </header>
+      <header className="App-header"></header>
+      <HeroeList heroes={heroes}></HeroeList>
     </div>
   );
 }
